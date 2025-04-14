07b95f9bbfcd8f0591e877632ea681cca4908b64
julieMaiaop <167346442+julieMaiaop@users.noreply.github.com>
2025-04-14 14:10:44 -0300

    add assets + mecanicas inicias

---
+++ PI-JulieESofia/Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Velocidade de movimento
+    public Rigidbody2D rb;       // Referência ao Rigidbody2D
+
+    Vector2 movement; // Vetor de movimento
+
+    void Update()
+    {
+        // Entrada do teclado (Eixo horizontal e vertical)
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Movimento aplicado ao Rigidbody2D
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
